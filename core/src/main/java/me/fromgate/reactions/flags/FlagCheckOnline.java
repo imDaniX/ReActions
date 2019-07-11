4541b2a7a57feb7db1cc9e1d074a21805adf7efc
imdanix <idanix@list.ru>
2019-07-12 02:17:18 +0300

    Deprecation \o/

--- core/src/main/java/me/fromgate/reactions/flags/FlagCheckOnline.java
+++ core/src/main/java/me/fromgate/reactions/flags/FlagCheckOnline.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class FlagCheckOnline extends Flag {
+	@SuppressWarnings("deprecation")
 	@Override
 	public boolean checkFlag(Player player, String param) {
 		return Bukkit.getPlayerExact(param) != null;
