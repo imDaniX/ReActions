724ff661fd64ec617f9cff87105a08951614ae28
Xezard <sanya.zikranets@yandex.ru>
2019-09-25 11:12:52 +0400

    That's better?

--- core/src/main/java/me/fromgate/reactions/placeholders/Placeholder.java
+++ core/src/main/java/me/fromgate/reactions/placeholders/Placeholder.java
@@ -3,42 +3,43 @@ package me.fromgate.reactions.placeholders;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 
-public abstract class Placeholder {
+@Getter
+public abstract class Placeholder 
+{
+    private final String id = "UNKNOWN";
+ 
+    private final String[] keys = new String[] {};
 
-	@Getter private final String id;
-	@Getter private final String[] keys;
+    public Placeholder() 
+    {
+        if (this.getClass().isAnnotationPresent(PlaceholderDefine.class)) 
+        {
+            PlaceholderDefine pd = this.getClass().getAnnotation(PlaceholderDefine.class);
+    
+            this.id = pd.id();
+            this.keys = pd.keys();
+        }
+    }
 
-	public Placeholder() {
-		if (this.getClass().isAnnotationPresent(PlaceholderDefine.class)) {
-			PlaceholderDefine pd = this.getClass().getAnnotation(PlaceholderDefine.class);
-			this.id = pd.id();
-			this.keys = pd.keys();
-		} else {
-			id = "UNKNOWN";
-			keys = new String[]{};
-		}
-	}
+    protected boolean equalsIgnoreCase(String key, String... values) 
+    {
+        return Stream.of(values)
+                     .anyMatch((string) -> string.equalsIgnoreCase(key));
+    }
 
-	protected boolean equalsIgnoreCase(String key, String... values) {
-		for (String s : values)
-			if (key.equalsIgnoreCase(s)) return true;
-		return false;
-	}
+    public boolean checkKey(String key) 
+    {
+        return Stream.of(this.keys)
+                     .anyMatch((k) -> k.equalsIgnoreCase(key));
+    }
 
-	public boolean checkKey(String key) {
-		for (String k : this.getKeys()) {
-			if (k.equalsIgnoreCase(key)) return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Замена ключеового слова
-	 *
-	 * @param player - игрок, если он есть
-	 * @param key	- Ключевое слово, без параметра и символа "%" в начале
-	 * @param param  - Параметр (без завершающего символа "%")
-	 * @return - возврат подстановки.
-	 */
-	public abstract String processPlaceholder(Player player, String key, String param);
+    /**
+     * Замена ключеового слова
+     *
+     * @param player - игрок, если он есть
+     * @param key - Ключевое слово, без параметра и символа "%" в начале
+     * @param param  - Параметр (без завершающего символа "%")
+     * @return - возврат подстановки.
+     */
+    public abstract String processPlaceholder(Player player, String key, String param);
 }
