a785abb5117c767937401956c0b1014d1c6007cc
imdanix <idanix@list.ru>
2019-09-25 14:41:09 +0300

    Import

--- core/src/main/java/me/fromgate/reactions/placeholders/Placeholder.java
+++ core/src/main/java/me/fromgate/reactions/placeholders/Placeholder.java
@@ -3,37 +3,33 @@ package me.fromgate.reactions.placeholders;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 
+import java.util.stream.Stream;
+
 @Getter
-public abstract class Placeholder 
-{
-    private String id = "UNKNOWN";
- 
-    private String[] keys = new String[] {};
+public abstract class Placeholder {
+	private String id = "UNKNOWN";
 
-    public Placeholder() 
-    {
-        if (this.getClass().isAnnotationPresent(PlaceholderDefine.class)) 
-        {
-            PlaceholderDefine pd = this.getClass().getAnnotation(PlaceholderDefine.class);
-    
-            this.id = pd.id();
-            this.keys = pd.keys();
-        }
-    }
+	private String[] keys = new String[] {};
 
-    public boolean checkKey(String key) 
-    {
-        return Stream.of(this.keys)
-                     .anyMatch((k) -> k.equalsIgnoreCase(key));
-    }
+	public Placeholder() {
+		if (this.getClass().isAnnotationPresent(PlaceholderDefine.class)) {
+			PlaceholderDefine pd = this.getClass().getAnnotation(PlaceholderDefine.class);
+			this.id = pd.id();
+			this.keys = pd.keys();
+		}
+	}
 
-    /**
-     * Замена ключеового слова
-     *
-     * @param player - игрок, если он есть
-     * @param key - Ключевое слово, без параметра и символа "%" в начале
-     * @param param  - Параметр (без завершающего символа "%")
-     * @return - возврат подстановки.
-     */
-    public abstract String processPlaceholder(Player player, String key, String param);
+	public boolean checkKey(String key) {
+		return Stream.of(this.keys).anyMatch(key::equalsIgnoreCase);
+	}
+
+	/**
+	 * Замена ключеового слова
+	 *
+	 * @param player - игрок, если он есть
+	 * @param key - Ключевое слово, без параметра и символа "%" в начале
+	 * @param param  - Параметр (без завершающего символа "%")
+	 * @return - возврат подстановки.
+	 */
+	public abstract String processPlaceholder(Player player, String key, String param);
 }
